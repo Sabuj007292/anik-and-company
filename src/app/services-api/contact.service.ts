a204012bb3613f0e22b5a992cb2f5546adbed5c1
Sabuj Gain <green.sabuj.2012@gmail.com>
2026-09-18 00:35:29 +0530

    update

--- src/app/services-api/contact.service.ts
+++ src/app/services-api/contact.service.ts
@@ -1,26 +1,95 @@
+// // import { Injectable } from '@angular/core';
+
+// // @Injectable({
+// //   providedIn: 'root'
+// // })
+// // export class ContactService {
+
+// // constructor() { }
+
+// // }
 // import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
 
 // @Injectable({
 //   providedIn: 'root'
 // })
 // export class ContactService {
 
-// constructor() { }
+//   private apiUrl = 'http://localhost:5000/api/contact';
 
+//   constructor(private http: HttpClient) {}
+
+//   saveContact(data: any) {
+//     return this.http.post(this.apiUrl, data);
+//   }
 // }
-import { Injectable } from '@angular/core';
+
+
+import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface ContactEnquiry {
+  _id?: string;
+  fullName: string;
+  mobile: string;
+  email: string;
+  location: string;
+  service: string;
+  message: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
+
+export interface ContactResponse {
+  success: boolean;
+  message?: string;
+  count?: number;
+  data?: ContactEnquiry[];
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
-  private apiUrl = 'http://localhost:5000/api/contact';
+  private readonly http = inject(HttpClient);
 
-  constructor(private http: HttpClient) {}
+  private readonly API_URL =
+    'https://www.anikandco.com/api/contact';
 
-  saveContact(data: any) {
-    return this.http.post(this.apiUrl, data);
+  /**
+   * Create new contact enquiry
+   */
+  createContact(
+    enquiry: ContactEnquiry
+  ): Observable<ContactResponse> {
+
+    return this.http.post<ContactResponse>(
+      this.API_URL,
+      enquiry,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      }
+    );
+  }
+
+  /**
+   * Get all contact enquiries
+   */
+  getContacts(): Observable<ContactResponse> {
+
+    return this.http.get<ContactResponse>(
+      this.API_URL,
+      {
+        headers: {
+          'Accept': 'application/json'
+        }
+      }
+    );
   }
 }
